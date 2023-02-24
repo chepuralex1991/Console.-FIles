a885fa81dd7dfe6696b2703565f37253f828165a
Oleksii Chepur <chepuralex1991@gmail.com>
2023-02-24 10:27:51 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+﻿import random
+import csv
+
+
+players = ["Josh", "Luke", "Kate", "Mark", "Mary"]
+
+game_results = []
+for player in players:
+    for i in range(10):
+        score = random.randint(0, 100)
+        game_results.append([player, score])
+random.shuffle(game_results)
+
+
+with open("game_results.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Player name", "Score"])
+    writer.writerows(game_results)
---
+++ main_high.py
@@ -0,0 +1,12 @@
+﻿import csv
+
+with open('game_results.csv', 'r') as data:
+    reader = csv.reader(data)
+    next(reader)
+    sorted_data = sorted(reader, key=lambda row: int(row[1]), reverse=True)[:5]
+        
+       
+with open("highest_game_results.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Player name", "Highest Score"])
+    writer.writerows(sorted_data)
